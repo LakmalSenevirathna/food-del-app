1acee35d592140d4b291e90f792e229906185e49
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-04-09 19:58:59 +0530

    Add header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './pages/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
