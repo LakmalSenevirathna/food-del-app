cfef72f34e1a7802f3f6b7ccc390d280569ab555
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-04-10 00:49:24 +0530

    Add menu items

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExporeMenu/ExploreMenu'
+import { useState } from 'react'
 
 const Home = () => {
+
+  const [category,setCategory] = useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
